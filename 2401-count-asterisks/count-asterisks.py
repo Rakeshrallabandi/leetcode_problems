d3139641e1afbd4f78a2612027affa823364baae
Rakesh Rallabandi <149596036+Rakeshrallabandi@users.noreply.github.com>
2025-08-30 23:47:21 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (14.40%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countAsterisks(self, s: str) -> int:
+        l=s.split('|')
+        ans=0
+        for i in range(len(l)):
+            if i%2==0:
+                ans+=l[i].count('*')
+        return ans
