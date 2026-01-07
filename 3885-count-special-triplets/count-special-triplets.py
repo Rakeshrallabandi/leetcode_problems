680b70df21c447cd1a5f27480db4d777af2389c5
Rakesh Rallabandi <149596036+Rakeshrallabandi@users.noreply.github.com>
2026-01-07 21:08:02 +0530

    Time: 496 ms (79.09%) | Memory: 42.4 MB (29.35%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.py
+++ 3885-count-special-triplets/count-special-triplets.py
@@ -5,7 +5,7 @@ class Solution:
         count=0
         for i in nums:
             r[i]=r.get(i,0)+1
-        print(r)
+    
         for i in nums:
             r[i]-=1
             if r[i]==0:
@@ -14,6 +14,5 @@ class Solution:
                 count+=(l[i*2]*r[i*2])
                 count%=10**9 + 7
             l[i]=l.get(i,0)+1
-            
-        print(r)
+
         return count
