1c5b92dc2ad0d25f807f25a45676b4a79cb474d5
Rakesh Rallabandi <149596036+Rakeshrallabandi@users.noreply.github.com>
2026-01-07 21:07:36 +0530

    Time: 582 ms (68.60%) | Memory: 42.6 MB (28.25%) - LeetSync

---
+++ 3885-count-special-triplets/count-special-triplets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        l={}
+        r={}
+        count=0
+        for i in nums:
+            r[i]=r.get(i,0)+1
+        print(r)
+        for i in nums:
+            r[i]-=1
+            if r[i]==0:
+                del r[i]
+            if i*2 in l and i*2 in r:
+                count+=(l[i*2]*r[i*2])
+                count%=10**9 + 7
+            l[i]=l.get(i,0)+1
+            
+        print(r)
+        return count
